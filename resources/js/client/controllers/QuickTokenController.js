0a0cb0ea8a4f775d368d3cb0473ca97fe362f1e3
S15SoKo <s15soko@onet.pl>
2019-08-21 15:50:55 +0200

    General func for quick token generate token save in db set in header

--- resources/js/client/controllers/QuickTokenController.js
+++ resources/js/client/controllers/QuickTokenController.js
@@ -2,14 +2,46 @@ import {RandomStringController} from './RandomStringController';
 
 export class QuickTokenController
 {
+    /**
+     * Create, save and set token in header
+     */
+    static create()
+    {
+        // generate token
+        let quickToken = QuickTokenController.generateToken();
+        QuickTokenController.saveInDatabase(quickToken);
+        // set quick token header
+        axios.defaults.headers.common.quickToken = quickToken;
+    }
+
+    /**
+     * Generate token
+     * 
+     * @return {string}
+     */
     static generateToken()
     {
         var token = RandomStringController.randomString(64);
         return token;
     }
 
+    /**
+     * Save token
+     * 
+     * @param {string} token 
+     */
     static saveInDatabase(token)
     {
         axios.post('/pusher/chatkit/quicktoken/save', qs.stringify({token: token}));
+    }
+
+    /**
+     * Set token in header 
+     * 
+     * @param {string} token 
+     */
+    static setHeader(token)
+    {
+        axios.defaults.headers.common.quickToken = quickToken;
     }
 }
