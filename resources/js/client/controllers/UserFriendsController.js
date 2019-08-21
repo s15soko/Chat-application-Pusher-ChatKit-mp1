11502fee05a67749ab817d238af6449c1615f840
S15SoKo <s15soko@onet.pl>
2019-08-21 22:11:10 +0200

    Return user data by id func

--- resources/js/client/controllers/UserFriendsController.js
+++ resources/js/client/controllers/UserFriendsController.js
@@ -5,4 +5,24 @@ export class UserFriendsController
     {
         return axios.post("/user/friends/get");
     }
+
+    /**
+     * Get friend data from list by his id
+     * 
+     * @param {int} requireID 
+     * @param {object} friends 
+     * 
+     * @return {object}
+     */
+    static returnFriendDataByID(requireID, friends)
+    {
+        var data = [];
+        friends.forEach(friend => {
+            if(Number(friend.id) === Number(requireID))
+                data = friend;
+                
+        });
+
+        return data;
+    }
 }
