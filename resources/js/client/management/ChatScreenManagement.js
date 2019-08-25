1ab2322f420814a57870fd1307e04e14286d42e1
S15SoKo <s15soko@onet.pl>
2019-08-25 20:03:42 +0200

    Code improvement + add few events for emojis

--- resources/js/client/management/ChatScreenManagement.js
+++ resources/js/client/management/ChatScreenManagement.js
@@ -5,6 +5,9 @@ import { SidebarController } from "../controllers/SidebarController";
 import { QuickTokenController } from "../controllers/QuickTokenController";
 import { MessagesController } from "../controllers/MessagesController";
 import { RoomsController } from "../controllers/RoomsController";
+import { LastMessageController } from "../controllers/LastMessageController";
+import { CurrentChatController } from "../controllers/CurrentChatController";
+import { EmojiPanelController } from "../controllers/EmojiPanelController";
 
 const UserIDController = require("../../client-server/UserIDController");
 const RoomIDController = require("../../client-server/RoomIDController");
@@ -14,9 +17,10 @@ export class ChatScreenManagement
     constructor()
     {
         // Actuall chatting in room...
-        this.actuallChattingIn = {
-            roomChatkitID: "",
-            roomID: ""
+        this.actuallChattingRoom = {
+            roomChatkitID: "", // string
+            roomID: "", // int,
+            members: [] 
         };
 
         // pusher chatkit object
@@ -26,7 +30,7 @@ export class ChatScreenManagement
         this.currentUserChatkit = null;
 
         // current logged user data
-        this.currentUser = {
+        this.currentLoggedUser = {
             id: null,
             avatar: "",
             rooms: {} // rooms he belong to
@@ -54,69 +58,76 @@ export class ChatScreenManagement
         // get current user data
         UserDataController.get()
             .then(function(userData){
-                management.currentUser = userData['data'];
+                management.currentLoggedUser = userData['data'];
 
                 // get user friends
                 UserFriendsController.getFriends()
                     .then(function(response){
-                        if(response['data'].length > 0){
-                            var friends = response['data'];
-            
-                            management.userFriends = friends;
-                            SidebarController.build(friends)
-                        }else{
-                        // add some friends 
-                        }
+                        var friends = response['data'];
+        
+                        // fill sidebar
+                        SidebarController.build(friends, management.currentLoggedUser);
+        
+                        management.userFriends = friends;
                     });
 
-                // init/ auth chat
+
+                // init chat manager
                 management.chatManager = new ChatManager({
                     instanceLocator: 'v1:us1:4f3fcbda-acc9-415e-86df-621a6b34ebe4',
-                    userId: String(UserIDController.addPrefix(userData.data.id)),
+                    userId: String(UserIDController.addPrefix(management.currentLoggedUser.id)),
                     connectionTimeout: 5000,
                     tokenProvider: new TokenProvider({ url: 'http://localhost:3000/pusher/chatkit/auth' })
-                }); 
+                });
 
-                // Connect with chat
+                // connect
                 management.chatManager.connect()
                     .then(currentUser => {
-                        currentUser.avatarURL = management.currentUser.avatar;
 
-                        // generate token
-                        let quickToken = QuickTokenController.generateToken();
-                        QuickTokenController.saveInDatabase(quickToken);
-                        // set quick token header
-                        axios.defaults.headers.common.quickToken = quickToken;
-                        
+                        QuickTokenController.create(); 
                         management.userConnected = true;
                         management.currentUserChatkit = currentUser;
 
                         // SET HOOKS
                         var firstRoom = true;
-                        management.currentUser.rooms.forEach(room => {
+                        // for each room
+                        management.currentLoggedUser.rooms.forEach(room => {
 
                             if(firstRoom){
                                 management.chatIn(room.room_id, room.id);
+                                
                                 firstRoom = false;
+                                SidebarController.markFirstRoomOnList();
+
+                                management.updateRoomMembers();
+
+                                CurrentChatController.showChatTopUserData(
+                                    management.getFriendData(
+                                        SidebarController.getFirstFromList()));
+
+                                MessagesController.stackMessages(room.id, management.currentLoggedUser);
                             }
 
                             management.subscribeRoom(room.room_id)
                                 .catch(function(err){
-                                    // try again 
-                                    if(err.info.error == "services/chatkit/not_found/room_not_found"){
+                                    if(err.info.error == "services/chatkit/not_found/room_not_found")
+                                    {
+                                        // try to create room
+                                        // and connent one more time
                                         RoomsController.checkByRoomID(room.id)
                                             .then(function(response){
                                                 if(response == 200 || response == 201){
                                                     management.subscribeRoom(room.room_id);
                                                 };
-                                            })
+                                            });
                                     }
                                 })
-                            
+                                
+                            });
                         });
-                    });
             });
 
+        EmojiPanelController.fillEmojiContainer();
         this.events();
     }
 
@@ -124,6 +135,8 @@ export class ChatScreenManagement
      * Subscribe room
      * 
      * @param {string} roomChatkitID 
+     * 
+     * @return {Promise}
      */
     subscribeRoom(roomChatkitID)
     {
@@ -134,13 +147,61 @@ export class ChatScreenManagement
                 roomId: String(roomChatkitID),
                 hooks: {
                   onMessage: message => {
-                      // sprawdz czy jest obecnie w tym okienku czatu
-                      // jesli tak to pokaz wiadomosc a jesli nie to pokaz alert
-                    MessagesController.build(message, management.currentUser.id);
+
+                    var senderID = UserIDController.removePrefix(message.senderId);
+                    var senderData = UserFriendsController.returnFriendDataByID(senderID, management.userFriends);
+
+                    MessagesController.onNewChatKitMessage(message, management.actuallChattingRoom, management.currentLoggedUser, senderData);
+                  },
+                  onPresenceChanged: (state, user) => {
+                    management.setFriendState(state, user);
+                    management.updateFriendState(UserIDController.removePrefix(user.id));
                   }
                 },
-                messageLimit: 0 // load previous messages from database
+                // load previous messages from database
+                messageLimit: 0 
             });
+    }
+
+    /**
+     * Set friend state
+     * 
+     * @param {object} state 
+     * @param {object} user 
+     */
+    setFriendState(state, user)
+    {
+        var management = this;
+        var userID = UserIDController.removePrefix(user.id);
+
+        management.userFriends.forEach(friend => {
+            if(friend.id == userID){
+                friend.state = state;
+                return;
+            }
+        });
+    }
+
+    /**
+     * Update user state {online, offline} in chat navbar
+     * 
+     * @param {int} userID 
+     */
+    updateFriendState(userID)
+    {
+        var management = this;
+        if(userID == management.currentLoggedUser.id 
+            || !RoomsController.userInRoom(management.actuallChattingRoom, UserIDController.addPrefix(userID)) )
+                return;
+
+        var friend = UserFriendsController.returnFriendDataByID(userID, management.userFriends);
+        CurrentChatController.showStatus(friend.state);
+    }
+
+    updateRoomMembers()
+    { 
+        var roomChatkitID = this.actuallChattingRoom.roomChatkitID;
+        this.actuallChattingRoom.members = RoomsController.getOneToOneMembers(roomChatkitID);
     }
 
     /**
@@ -151,8 +212,9 @@ export class ChatScreenManagement
      */
     chatIn(roomChatkitID, roomID)
     {
-        this.actuallChattingIn.roomChatkitID = String(roomChatkitID);
-        this.actuallChattingIn.roomID = Number(roomID);
+        var management = this;
+        management.actuallChattingRoom.roomChatkitID = String(roomChatkitID);
+        management.actuallChattingRoom.roomID = Number(roomID);
     }
 
     /**
@@ -165,15 +227,24 @@ export class ChatScreenManagement
             return;
 
         var data = {
-            roomChatkitID: String(management.actuallChattingIn.roomChatkitID),
-            roomID: Number(management.actuallChattingIn.roomID),
-            userID: Number(management.currentUser.id),
+            roomChatkitID: String(management.actuallChattingRoom.roomChatkitID),
+            roomID: Number(management.actuallChattingRoom.roomID),
+            userID: Number(management.currentLoggedUser.id),
             message: $("#chatMessageTextarea").val(),
         }
 
-        MessagesController.send(data);
+        MessagesController.send(data)
+            .then(function(res){
+                //
+            })
+            .finally(function(){
+                $("#chatMessageTextarea").val("");
+            })
     }
 
+    /**
+     * Default chat events
+     */
     events()
     {
         var management = this;
@@ -182,26 +253,45 @@ export class ChatScreenManagement
             management.sendMessage();
         });
 
-        $(document).on("click", "#test", function(){
-            management.test();
-        });
-
         $(document).on('click', ".friend", function(e){
             management.changeChatScreen(e);
         });
+
+        $(document).on('click', "#chatEmojiFace", function(e){
+            $("#emojisContainer").toggleClass("active");
+        });
+
+        $(document).on('click', "#emojisContainer .icon", function(e){
+            var emoji = $(this).text();
+            var content = $("#chatMessageTextarea").val();
+            $("#chatMessageTextarea").val(content + emoji);
+        });
     }
 
+    /**
+     * If the user change the person who is talking to
+     * 
+     * @param {object} e || the object the user clicked on
+     */
     changeChatScreen(e)
     {
         var management = this;
-        $("#messages").empty();
-        var element = $(e.target).closest(".friend");
-        element.addClass("active");
+        SidebarController.unmarkAllActiveRooms()
 
-        var roomChatkitID = RoomIDController.createForOneToOne(management.currentUser.id, element.attr("data-user-id"));
+        var element = $(e.target).closest(".friend");
+        SidebarController.markActiveRoom(element);
+
+        $("#messages").empty();
+
+        var chatWithUserID = element.attr("data-user-id");
+        CurrentChatController.showChatTopUserData(management.getFriendData(chatWithUserID));
+        LastMessageController.unmarkRoom(chatWithUserID);
+
+        var roomChatkitID = RoomIDController.createForOneToOne(management.currentLoggedUser.id, chatWithUserID);
         var roomID = null;
-        
-        management.currentUser.rooms.forEach(roomIndex => {
+
+        // get room id for sql
+        management.currentLoggedUser.rooms.forEach(roomIndex => {
             if(roomIndex.room_id === roomChatkitID){
                 roomID = roomIndex.id;
                 return;
@@ -209,26 +299,22 @@ export class ChatScreenManagement
         });
 
         management.chatIn(roomChatkitID, roomID);
+        management.updateRoomMembers();
+        management.updateFriendState(chatWithUserID);
+        MessagesController.stackMessages(roomID, management.currentLoggedUser);
     }
 
-    test()
+    /**
+     * Get friend data by his ID
+     * 
+     * @param {int} userID 
+     * 
+     * @return {object}
+     */
+    getFriendData(userID)
     {
-        console.log('this.actuallChattingIn');
-        console.log(this.actuallChattingIn);
-
-        console.log('this.chatManager');
-        console.log(this.chatManager);
-
-        console.log('this.currentUserChatkit');
-        console.log(this.currentUserChatkit);
-
-        console.log('this.currentUser');
-        console.log(this.currentUser);
-
-        console.log('this.userFriends');
-        console.log(this.userFriends);
-
-        console.log('this.userConnected');
-        console.log(this.userConnected);
+        var management = this;
+        var friend = UserFriendsController.returnFriendDataByID(userID, management.userFriends);
+        return friend;
     }
 }
